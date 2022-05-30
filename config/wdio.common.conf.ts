bdbfac07d2821bae6cb9783599e6b7cd476ab01a
ulises-jeremias <ulisescf.24@gmail.com>
2022-05-30 01:50:59 -0300

    Updated runs

--- config/wdio.common.conf.ts
+++ config/wdio.common.conf.ts
@@ -1,4 +1,5 @@
 import { getWebdriverLogLevel, getWebdriverOutputDir, getWebdriverSpecFileRetries } from './env';
+import allure from 'allure-commandline';
 
 export const config: Partial<WebdriverIO.Config> = {
   logLevel: getWebdriverLogLevel(),
@@ -9,7 +10,15 @@ export const config: Partial<WebdriverIO.Config> = {
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
   framework: 'mocha',
-  reporters: [['allure', { outputDir: 'reports' }]],
+  reporters: [
+    [
+      'allure',
+      {
+        outputDir: 'allure-results',
+        addConsoleLogs: true,
+      },
+    ],
+  ],
   mochaOpts: {
     ui: 'bdd',
     timeout: 60000,
@@ -22,8 +31,23 @@ export const config: Partial<WebdriverIO.Config> = {
       await browser.takeScreenshot();
     }
   },
-  onComplete: async function () {
-    console.log('// Test execution has completed //');
+  onComplete: function () {
+    const reportError = new Error('Could not generate Allure report');
+    const generation = allure(['generate', 'allure-results', '--clean']);
+    return new Promise<void>((resolve, reject) => {
+      const generationTimeout = setTimeout(() => reject(reportError), 5000);
+
+      generation.on('exit', function (exitCode: number) {
+        clearTimeout(generationTimeout);
+
+        if (exitCode !== 0) {
+          return reject(reportError);
+        }
+
+        console.log('Allure report successfully generated');
+        resolve();
+      });
+    });
   },
   onPrepare: async function () {
     console.log('// Test execution has started //');
--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,8 +1,11 @@
 import LoginPage from '../pageobjects/login.page';
 import SecurePage from '../pageobjects/secure.page';
+import allureReporter from '@wdio/allure-reporter';
 
 describe('My Login application', () => {
   it('should login with valid credentials', async () => {
+    allureReporter.addFeature('Login');
+
     await LoginPage.open();
 
     await LoginPage.login('tomsmith', 'SuperSecretPassword!');
